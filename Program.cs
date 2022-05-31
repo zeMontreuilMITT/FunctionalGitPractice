dc468307c088bbc9c9f45adb897a734660780891
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-05-31 10:58:41 -0500

    Lab 1 Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// my machine is LOCAL
-
-// GitHub is REMOTE
+﻿int WriteInts()
+{
+    return 1;
+}
